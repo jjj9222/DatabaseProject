191c1517ad6fa1f13061d9943b9bd561c60e3814
Jordan <32456763+jjj9222@users.noreply.github.com>
2020-10-21 10:24:41 -0400

    Add files via upload

--- database_change.py
+++ database_change.py
@@ -1,38 +1,74 @@
 import psycopg2
-import tkinter as tk
-from tkinter import simpledialog
+from tkinter import *
+
+hostname = 'reddwarf.cs.rit.edu'
+username = 'p320_02'
+password = 'Eux5iothoo3WaeL7yahM'
+database = 'p320_02'
+
+# create connection to db
+connection = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
+
+# Cursor for the database
+cursor = connection.cursor()
+
+def getQuery():
+    print(song_var.get())
+    if (song_var == ""):
+        pass
+    elif (song_var == "*"):
+        print("in")
+        cursor.execute("""SELECT title from song""")
+        data = cursor.fetchall()
+        for row in data:
+            song_listbox.insert(END, row[0])
+    else:
+        try:
+            query = "SELECT title from song where title like %" + song_var + "%"
+            cursor.execute(query)
+            data = cursor.fetchall()
+            for row in data:
+                song_listbox.insert(END, row[0])
+        except:
+            print("Invalid Query")
 
 
-if __name__ == '__main__':
-    # Connection to the database
-    connection = psycopg2.connect(host="reddwarf.cs.rit.edu", database="p320_02", user="p320_02", password="Eux5iothoo3WaeL7yahM")
+window = Tk()
+window.title("Music Player")
+window.geometry("1000x500")
 
-    # Cursor for the database
-    cursor = connection.cursor()
+#Getting Song Browser Setup
+song_label = Label(window, text="Song Name")
+song_label.grid(column=0)
+song_scoll = Scrollbar(window)
+song_scoll.grid(row=1, column=1, rowspan=10, sticky=N+S+W)
+song_listbox = Listbox(width=50, yscrollcommand=song_scoll.set)
+song_listbox.grid(row=1)
+song_scoll.config(command=song_listbox.yview)
 
-    cursor.execute("""SELECT title from song""")
+artist_label = Label(window, text="Artist Name")
+artist_label.grid(column=20, row=0)
+artist_scoll = Scrollbar(window)
+artist_scoll.grid(row=1, column=20, rowspan=10)
+artist_listbox = Listbox(width=50, yscrollcommand=artist_scoll.set)
+artist_listbox.grid(row=1, column=20)
+artist_scoll.config(command=artist_listbox.yview)
 
-    data = cursor.fetchall()
+album_label = Label(window, text="Album Name")
+album_label.grid(column=42, row=0)
+album_scoll = Scrollbar(window)
+album_scoll.grid(row=1, column=42, rowspan=10)
+album_listbox = Listbox(width=50, yscrollcommand=album_scoll.set)
+album_listbox.grid(row=1, column=42)
+album_scoll.config(command=album_listbox.yview)
 
-    window = tk.Tk()
-    window.title("Music Collection Interface")
-    window.rowconfigure(0, minsize=3000, weight=1)
-    window.columnconfigure(0, minsize=3000, weight=1)
-    window.geometry("1000x500")
-    song_label = tk.Label(text="Song Name")
-    entry = tk.Entry()
-    # Making the list that displays the songs
-    song_listbox = tk.Listbox(width=50)
-    album_listbox = tk.Listbox()
+song_var = StringVar()
 
-    song_label.pack()
-    song_listbox.pack()
+song_entry = Entry(window, textvariable=song_var)
+song_entry.grid(row=10)
 
+but = Button(window, text="Search", command=getQuery)
+but.grid(row=15)
 
-
-    song_listbox.insert(0, "a list entry")
-    for row in data:
-        song_listbox.insert(tk.END, row[0])
-
-    window.mainloop()
+window.mainloop()
 
