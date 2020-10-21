d50fd2c37dfcd980c95db426d573536b1339992b
Wendy Liu <wl8442@rit.edu>
2020-10-21 00:26:08 -0400

    only display results from most recent query

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 from __future__ import print_function
 import psycopg2
 from tkinter import *
+import time
 
 hostname = 'reddwarf.cs.rit.edu'
 username = 'p320_02'
 password = 'Eux5iothoo3WaeL7yahM'
 database = 'p320_02'
+
+empty = True
 
 # create connection to db
 myConn = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
@@ -13,7 +16,7 @@ myConn = psycopg2.connect(host=hostname, user=username, password=password, dbnam
 # create initial window
 top = Tk()
 top.title("Music Player")
-top.geometry("300x500")
+top.geometry("300x300")
 
 query_var = StringVar()
 
@@ -24,6 +27,14 @@ def doQuery(conn, query_in):
     :param conn: connection to db
     :param query_in: sql query as entered by user
     """
+
+    global empty
+    empty = False
+    if query_in == "":
+        error = Label(top, text="Cannot execute empty query")
+        error.grid(row=2, column=0)
+        time.sleep(3)
+        exit()
     cur = conn.cursor()
     cur.execute(query_in)
 
@@ -33,6 +44,12 @@ def doQuery(conn, query_in):
 
 def getQuery():
     global query_var
+    global empty
+
+    if not empty:
+        lb.delete(0, END)
+        empty = True
+
     query = query_var.get()
 
     doQuery(myConn, query)
@@ -51,17 +68,16 @@ def displayQuery(elem):
 
 # elements of the window
 lab = Label(top, text="Enter query")
-lab.grid(row=0)
+lab.grid(row=0, pady=5, padx=2)
 q = Entry(top, textvariable=query_var)
-q.grid(row=1)
+q.grid(row=1, pady=5, padx=2)
 but = Button(top, text="execute query", command=getQuery)
-but.grid(row=2)
+but.grid(row=2, pady=5, padx=2)
 scroll = Scrollbar(top)
 scroll.grid(row=3, column=1, rowspan=4, sticky=N+S+W)
 
 lb = Listbox(top, yscrollcommand=scroll.set)
 scroll.config(command=lb.yview)
-
 
 top.mainloop()
 myConn.close()
