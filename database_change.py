f64378658a1a6b37d4902f93ac7fad93165a8ad2
Jordan <32456763+jjj9222@users.noreply.github.com>
2020-10-22 12:08:52 -0400

    Add files via upload

--- database_change.py
+++ database_change.py
@@ -15,26 +15,40 @@ cursor = connection.cursor()
 def getSongQuery():
     # Deletes the info in the listbox on press of button
     song_listbox.delete(0, END)
-    #list = song_listbox.get(song_listbox.curselection())
-    #print(list)
 
-    if (song_var.get() == ""):
-        pass
-    elif (song_var.get() == "*"):
+
+    # Elif statement used to display all songs when user enters * or nothing
+    # across the other lists, corresponding artist and albums will be displayed if a song is created by one or a song is in album
+    if (song_var.get() == "*" or song_var.get() == ""):
         cursor.execute("""SELECT title from song""")
         data = cursor.fetchall()
         for row in data:
             song_listbox.insert(END, row[0])
 
         #display all artists that sing songs, all albums that have songs in them
-        query = "SELECT A.\"firstName\", A.\"lastName\" from artist A, song S, performed P where P.\"artistID\" = A.\"artistID\" AND P.id = S.id"
+        query = "SELECT DISTINCT A.\"firstName\", A.\"lastName\" from artist A, song S, performed P where P.\"artistID\" = A.\"artistID\" AND P.id = S.id"
         cursor.execute(query)
         data = cursor.fetchall()
         artist_listbox.delete(0, END)
         for row in data:
             artist_listbox.insert(END, row)
 
+        query = "SELECT DISTINCT A.\"albumName\" from album A, song S, contains C where A.\"albumID\" = C.\"albumID\" AND C.id = S.id"
+        cursor.execute(query)
+        data = cursor.fetchall()
+        album_listbox.delete(0, END)
+        for row in data:
+            album_listbox.insert(END, row[0])
+
+    # Else statement is for refined searches
+    # a user can enter text and a query will be populated
     else:
+        try:
+            query = "SELECT id from song where title like '%" + song_var.get() + "%'"
+            cursor.execute(query)
+            song_ids = cursor.fetchall()
+        except:
+            print("Failed to get song IDs")
         try:
             query = "SELECT title from song where title like '%" + song_var.get() + "%'"
             cursor.execute(query)
@@ -45,16 +59,24 @@ def getSongQuery():
             print("Invalid Query")
 
         try:
-            query = "SELECT id from song where title like '%" + song_var.get() + "%'"
-            cursor.execute(query)
-            data = cursor.fetchall()
             artist_listbox.delete(0, END)
-            for number in data:
+            for number in song_ids:
                 query = "SELECT DISTINCT A.\"firstName\", A.\"lastName\" from artist A, song S, performed P where P.\"artistID\" = A.\"artistID\" AND P.id = {0[0]}".format(number)
                 cursor.execute(query)
                 data = cursor.fetchall()
                 for row in data:
-                    artist_listbox.insert(END, row)
+                    artist_listbox.insert(END, row[0])
+        except:
+            print("Error")
+
+        try:
+            album_listbox.delete(0, END)
+            for number in song_ids:
+                query = "SELECT DISTINCT A.\"albumName\" from album A, song S, contains C where A.\"albumID\" = C.\"albumID\" AND C.id = {0[0]}".format(number)
+                cursor.execute(query)
+                data = cursor.fetchall()
+                for row in data:
+                    album_listbox.insert(END, row[0])
         except:
             print("Error")
 
@@ -63,16 +85,40 @@ def getArtistQuery():
     # Deletes the info in the listbox on press of button
     artist_listbox.delete(0, END)
 
-    if (artist_var.get() == ""):
-        pass
-    elif (artist_var.get() == "*"):
+    if (artist_var.get() == "*" or artist_var.get() == ""):
         cursor.execute("""SELECT "firstName", "lastName" from artist""")
         data = cursor.fetchall()
         for row in data:
             artist_listbox.insert(END, row)
+
+        # display all songs that an artist sings, all albums that have artist in them
+        query = "SELECT DISTINCT S.title from artist A, song S, performed P where P.\"artistID\" = A.\"artistID\" AND P.id = S.id"
+        cursor.execute(query)
+        data = cursor.fetchall()
+        song_listbox.delete(0, END)
+        for row in data:
+            song_listbox.insert(END, row[0])
+
+        query = "SELECT DISTINCT A.\"albumName\" from album A, artist S, produced P where A.\"albumID\" = P.\"albumID\" AND P.\"artistID\" = S.\"artistID\""
+        cursor.execute(query)
+        data = cursor.fetchall()
+        album_listbox.delete(0, END)
+        for row in data:
+            album_listbox.insert(END, row[0])
     else:
+        artistName = []
         try:
-            query = "SELECT \"firstName\", \"lastName\" from artist where \"firstName\" like '%" + artist_var.get() + "%' or \"lastName\" like '%" + artist_var.get() + "%'"
+            if(artist_var.get()[0] == "("):
+                artistName = artist_var.get()[1:len(artist_var.get())-1].split(',')
+                i = 0
+                while i < len(artistName):
+                    artistName[i] = artistName[i].replace(" ", "")
+                    artistName[i] = artistName[i].replace("\'", "")
+                    i += 1
+            if (len(artistName) != 0):
+                query = "SELECT \"firstName\", \"lastName\" from artist where \"firstName\" like '%" + artistName[0] + "%' AND \"lastName\" like '%" + artistName[1] + "%'"
+            else:
+                query = "SELECT \"firstName\", \"lastName\" from artist where \"firstName\" like '%" + artist_var.get() + "%' or \"lastName\" like '%" + artist_var.get() + "%'"
             cursor.execute(query)
             data = cursor.fetchall()
             for row in data:
@@ -80,27 +126,110 @@ def getArtistQuery():
         except:
             print("Invalid Query")
 
+
+        # Grab the artist id for use to compare to the relationship tables
+        if (len(artistName) != 0):
+            query = "SELECT \"artistID\" from artist where \"firstName\" like '%" + artistName[
+                0] + "%' AND \"lastName\" like '%" + artistName[1] + "%'"
+        else:
+            query = "SELECT \"artistID\" from artist where \"firstName\" like '%" + artist_var.get() + "%' or \"lastName\" like '%" + artist_var.get() + "%'"
+        cursor.execute(query)
+        artist_ids = cursor.fetchall()
+
+        try:
+            # displaying the songs that the artists sing
+            song_listbox.delete(0, END)
+            for number in artist_ids:
+                query = "SELECT DISTINCT S.title from song S, performed P where S.id = P.id AND P.\"artistID\" = {0[0]}".format(number)
+                cursor.execute(query)
+                data = cursor.fetchall()
+                for row in data:
+                    song_listbox.insert(END, row[0])
+        except:
+            print("Error")
+
+        try:
+            # displaying the albums that the artists make
+            album_listbox.delete(0, END)
+            for number in artist_ids:
+                query = "SELECT DISTINCT A.\"albumName\" from album A, produced P where A.\"albumID\" = P.\"albumID\" AND P.\"artistID\" = {0[0]}".format(number)
+                cursor.execute(query)
+                data = cursor.fetchall()
+                for row in data:
+                    album_listbox.insert(END, row[0])
+        except:
+            print("Error")
+
 def getAlbumQuery():
     # Deletes the info in the listbox on press of button
     album_listbox.delete(0, END)
 
-    if (album_var.get() == ""):
-        pass
-    elif (album_var.get() == "*"):
+    if (album_var.get() == "*" or album_var.get() == ""):
         cursor.execute("""SELECT "albumName" from album""")
         data = cursor.fetchall()
         for row in data:
-            album_listbox.insert(END, row)
+            album_listbox.insert(END, row[0])
+
+        # display all songs that are in albums
+        query = "SELECT DISTINCT S.title from album A, song S, contains C where C.\"albumID\" = A.\"albumID\" AND C.id = S.id"
+        cursor.execute(query)
+        data = cursor.fetchall()
+        song_listbox.delete(0, END)
+        for row in data:
+            song_listbox.insert(END, row[0])
+
+        #display all artists that have an album
+        query = "SELECT DISTINCT A.\"firstName\", A.\"lastName\" from artist A, album S, produced P where P.\"artistID\" = A.\"artistID\" AND P.\"albumID\" = S.\"albumID\""
+        cursor.execute(query)
+        data = cursor.fetchall()
+        artist_listbox.delete(0, END)
+        for row in data:
+            artist_listbox.insert(END, row)
+
+
     else:
+        # get a list of the album ID for comparison
+        try:
+            query = "SELECT \"albumID\" from album where \"albumName\" like '%" + album_var.get() + "%'"
+            cursor.execute(query)
+            album_ids = cursor.fetchall()
+        except:
+            print("Failed to get album IDs")
+
         try:
             query = "SELECT \"albumName\" from album where \"albumName\" like '%" + album_var.get() + "%'"
-            print(query)
             cursor.execute(query)
             data = cursor.fetchall()
             for row in data:
-                album_listbox.insert(END, row)
+                album_listbox.insert(END, row[0])
         except:
             print("Invalid Query")
+
+        #Display the songs that are in a given album
+        try:
+            song_listbox.delete(0, END)
+            for number in album_ids:
+                query = "SELECT DISTINCT S.title from song S, contains C where S.id = C.id AND C.\"albumID\" = {0[0]}".format(number)
+                cursor.execute(query)
+                data = cursor.fetchall()
+                for row in data:
+                    song_listbox.insert(END, row[0])
+        except:
+            print("Error")
+
+        #Display artist that was made the given album
+        try:
+            artist_listbox.delete(0, END)
+            for number in album_ids:
+                query = "SELECT DISTINCT A.\"firstName\", A.\"lastName\" from artist A, produced P where P.\"artistID\" = A.\"artistID\" AND P.\"albumID\" = {0[0]}".format(number)
+                cursor.execute(query)
+                data = cursor.fetchall()
+                for row in data:
+                    artist_listbox.insert(END, row[0])
+        except:
+            print("Error")
+
+
 
 def curSongSelect(event):
     widget = event.widget
@@ -109,7 +238,6 @@ def curSongSelect(event):
         song_var.set(widget.get(cursor_select[0]))
     except:
         pass
-    print(song_var)
 
 def curArtistSelect(event):
     widget = event.widget
