117abb5f58eada0f623e442023626c3bed57fa45
sed1550 <sarah.ejds@gmail.com>
2020-11-15 17:29:29 -0500

    Update countSongCollection()

--- collection.py
+++ collection.py
@@ -2,10 +2,17 @@ import csv
 
 def countSongCollection(cursor):
     # finds the most popularly added songs
-    query = "SELECT id, count(id) as count from collection group by id"
+    query = "SELECT s.id, r.title, count(s.id) as count " \
+            "from \"songCollection\" s, song r " \
+            "where s.id = r.id group by s.id, r.title"
     cursor.execute(query)
     data = cursor.fetchall()
     print(data)
 
     with open('collection_song.csv', mode='w') as song_file:
-        song_writer = csv.writer(song_file)
+        fields = ['id', 'title', 'count']
+        song_writer = csv.DictWriter(song_file, fieldnames=fields)
+        song_writer.writeheader()
+
+        for row in data:
+            song_writer.writerow({fields[0]: row[0], fields[1]: row[1], fields[2]: row[2]})
