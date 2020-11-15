8d51aa3f39474ab00f2b7bdc9f5abe20de38a6d5
sed1550 <sarah.ejds@gmail.com>
2020-11-15 18:22:31 -0500

    writes only top 10 songs

--- collection.py
+++ collection.py
@@ -4,7 +4,8 @@ def countSongCollection(cursor):
     # finds the most popularly added songs
     query = "SELECT s.id, r.title, count(s.id) as count " \
             "from \"songCollection\" s, song r " \
-            "where s.id = r.id group by s.id, r.title"
+            "where s.id = r.id group by s.id, r.title " \
+            "order by count desc"
     cursor.execute(query)
     data = cursor.fetchall()
     print(data)
@@ -14,5 +15,9 @@ def countSongCollection(cursor):
         song_writer = csv.DictWriter(song_file, fieldnames=fields)
         song_writer.writeheader()
 
+        count = 0
         for row in data:
+            if count == 10:
+                break
             song_writer.writerow({fields[0]: row[0], fields[1]: row[1], fields[2]: row[2]})
+            count += 1
