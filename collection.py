84b208d7118d211c4901c54ecf09c45db8a50007
sed1550 <sarah.ejds@gmail.com>
2020-11-16 15:03:21 -0500

    average user time

--- collection.py
+++ collection.py
@@ -1,4 +1,6 @@
 import csv
+import time
+import datetime
 
 def countSongCollection(cursor):
     # finds the most popularly added songs
@@ -21,3 +23,36 @@ def countSongCollection(cursor):
                 break
             song_writer.writerow({fields[0]: row[0], fields[1]: row[1], fields[2]: row[2]})
             count += 1
+
+
+def averageUserTime(cursor):
+    # finds the average time users spend on the application
+    today = datetime.datetime.today()
+    formatToday = today.strftime('%Y-%m-%d')
+    weekDays = [formatToday]
+    dictWeek = {}
+    avg = 0
+    ct = 0
+    for i in range(1, 7):
+        weekday = today - datetime.timedelta(days=i)
+        formatToday = weekday.strftime('%Y-%m-%d')
+        weekDays.append(formatToday)
+
+    for week in weekDays:
+        query = "select l.uid, avg(s.length) as avg " \
+                "from listens_to l, song s " \
+                "where timestamp like \'%" + week + "%\' AND l.id = s.id " \
+                                                           "group by l.uid"
+        cursor.execute(query)
+        data = cursor.fetchall()
+
+        for row in data:
+            if row[0] in dictWeek.keys():
+                dictWeek[row[0]] += row[1]
+            else:
+                dictWeek[row[0]] = row[1]
+
+    for key in dictWeek.keys():
+        val = dictWeek[key]/420
+        ct += 1
+        avg += val
